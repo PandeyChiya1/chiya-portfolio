f70eea41ca74cdae227619eaebfb6574c035e2d2
Chiya Pandey <145787386+PandeyChiya1@users.noreply.github.com>
2025-05-19 23:22:15 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
   return (
     <div className='grid gird-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] h-[70vh] mx-auto py-8 bg-black'>
       
-    
+    <div className='col-span-1 my-auto mx-auto w-[300px] md:h-auto lg:w-[400px]'>
+        <img src = {heroimage} alt="hero image"/>
+    </div>
 
 <div className='col-span-2 px-5 my-auto'>
 
